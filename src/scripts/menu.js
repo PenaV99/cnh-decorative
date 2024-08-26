e78176ef75b0ceeb4be9366fa9c8fdc52a38ba7d
Victor <vhpc24@proton.me>
2024-08-26 10:59:01 -0500

    Added components to every page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,12 +1,15 @@
-const hamburgerMenu = document.querySelector('#hamburger-menu')
-const navBar = document.querySelector('#nav-bar')
-const navLinks = document.querySelector('nav-items')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const navBar = document.querySelector('.nav-bar')
+const navLinks = document.querySelectorAll('.nav-items')
 
 hamburgerMenu.addEventListener('click', function() {
     navBar.classList.toggle('active')
     hamburgerMenu.classList.toggle('active')
 })
-    // navLinks.addEventListener('click', function() {
-    //     navBar.classList.toggle('active')
-    // })
 
+  for (let link of navLinks) {
+    link.addEventListener('click', function(){
+      navBar.className = 'nav-bar'
+      hamburgerMenu.className = 'hamburger-menu'
+    })
+  }
